e825fbe649b4a36d8936c3ff1291f275d1155c50
iPranav-Patil <spydycool@gmail.com>
2024-04-13 14:14:55 +0530

    Auth

--- frontend/app/student/login/page.jsx
+++ frontend/app/student/login/page.jsx
@@ -1,14 +1,19 @@
+"use client"
 import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const StudentLogin = () => {
+const TeacherLogin = () => {
+  const router = useRouter();
   return (
     <section class="bg-white">
-      <div class="grid grid-cols-1 lg:grid-cols-2">
+      <div class="grid grid-cols-1 lg:grid-cols-2 h-screen">
         <div class="relative flex items-end px-4 pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
           <div class="absolute inset-0">
             <img
               class="object-cover object-top w-full h-full"
-              src="https://cdn.rareblocks.xyz/collection/celebration/images/signin/4/girl-thinking.jpg"
+              src="https://skill.samsodisha.gov.in/studentmanagement/img/diplomacart.png"
+
               alt=""
             />
           </div>
@@ -108,24 +113,14 @@ const StudentLogin = () => {
 
         <div class="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
           <div class="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-            <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl">
-              Sign in to Celebration
+            <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl mb-4">
+              Sign in As a Student
             </h2>
-            <p class="mt-2 text-base text-gray-600">
-              Don’t have an account?{" "}
-              <a
-                href="#"
-                title=""
-                class="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
-              >
-                Create a free account
-              </a>
-            </p>
 
-            <form action="#" method="POST" class="mt-8">
+            <htmlForm action="#" method="POST" class="mt-8">
               <div class="space-y-5">
                 <div>
-                  <label for="" class="text-base font-medium text-gray-900">
+                  <label htmlFor="" class="text-base font-medium text-gray-900">
                     {" "}
                     Email address{" "}
                   </label>
@@ -139,9 +134,9 @@ const StudentLogin = () => {
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
                         />
                       </svg>
@@ -159,7 +154,7 @@ const StudentLogin = () => {
 
                 <div>
                   <div class="flex items-center justify-between">
-                    <label for="" class="text-base font-medium text-gray-900">
+                    <label htmlFor="" class="text-base font-medium text-gray-900">
                       {" "}
                       Password{" "}
                     </label>
@@ -183,9 +178,9 @@ const StudentLogin = () => {
                         stroke="currentColor"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
                         />
                       </svg>
@@ -209,44 +204,19 @@ const StudentLogin = () => {
                     Log in
                   </button>
                 </div>
+                <div className=" ">
+                  <button
+                    className="text-semibold "
+                    onClick={() => router.push('/teacher/login')}
+
+                  >
+                    Login As a <span className="text-blue-700">Student?</span>
+                  </button>
+                </div>
               </div>
-            </form>
+            </htmlForm>
 
-            <div class="mt-3 space-y-3">
-              <button
-                type="button"
-                class="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-              >
-                <div class="absolute inset-y-0 left-0 p-4">
-                  <svg
-                    class="w-6 h-6 text-rose-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                  </svg>
-                </div>
-                Sign in with Google
-              </button>
 
-              <button
-                type="button"
-                class="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-              >
-                <div class="absolute inset-y-0 left-0 p-4">
-                  <svg
-                    class="w-6 h-6 text-[#2563EB]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                  </svg>
-                </div>
-                Sign in with Facebook
-              </button>
-            </div>
           </div>
         </div>
       </div>
@@ -254,4 +224,4 @@ const StudentLogin = () => {
   );
 };
 
-export default StudentLogin;
+export default TeacherLogin;
