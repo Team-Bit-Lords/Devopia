bf9c8aafc9102a08d2a7de4d0ab844568f356aaa
Shaunak Rananaware <shaunakrananaware@gmail.com>
2024-04-13 19:27:56 +0530

    fetch_quizes

--- backend/src/controllers/students/fetch_quiz.py
+++ backend/src/controllers/students/fetch_quiz.py
@@ -2,9 +2,10 @@ from flask import request
 from ...db.connection import db
 from ...utils.ApiResponse import ApiResponse
 from ...utils.ApiError import ApiError
-from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
-def all_quizzes():
+@jwt_required()
+def previous_quizzes():
     email = get_jwt_identity()
     existUser = db.students.find_one({"email": email})
     if not existUser:
@@ -12,18 +13,3 @@ def all_quizzes():
     
     user_quizzes = existUser["quizzes"]
     return ApiResponse(200, user_quizzes).json
-
-def get_quiz():
-    quiz_id = request.get_json()["quiz_id"]
-    email = get_jwt_identity()
-
-    existUser = db.students.find_one({"email": email})
-
-    if not existUser:
-        return ApiError(403, "No such user exists").json
-    
-    elif existUser and existUser["class"] is not None:
-        quiz = [quiz for quiz in existUser["quizzes"] if quiz["id"] == quiz_id]
-        return ApiResponse(200, quiz).json
-
-    return ApiError(500, "There was some error with the server").json
--- backend/src/routes/student.py
+++ backend/src/routes/student.py
@@ -3,7 +3,7 @@ from ..controllers.students.login import login
 from ..controllers.students.signup import signup
 from ..controllers.students.courses import courses_name
 from ..controllers.students.quiz_submit import quiz_submit
-from ..controllers.students.fetch_quiz import all_quizzes, get_quiz
+from ..controllers.students.fetch_quiz import previous_quizzes
 from ..controllers.students.chatbot import chatbot
 
 student = Blueprint("student", __name__, url_prefix="/student")
@@ -12,6 +12,5 @@ student.add_url_rule("/login", view_func=login, methods=["POST"])
 student.add_url_rule("/signup", view_func=signup, methods=["POST"])
 student.add_url_rule("/courses", view_func=courses_name, methods=["GET"])
 student.add_url_rule("/quiz", view_func=quiz_submit, methods=["POST"])
-student.add_url_rule("/all_quizzes", view_func=all_quizzes, methods=["GET"])
-student.add_url_rule("/previous_quizzes", view_func=get_quiz, methods=["POST"])
+student.add_url_rule("/all_quizzes", view_func=previous_quizzes, methods=["GET"])
 student.add_url_rule("/chatbot", view_func=chatbot, methods=["POST"])
