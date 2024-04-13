5356c1a6c7ba49867cea6be70990e0fa7392b712
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-04-13 18:14:10 +0530

    Add DaisyUI plugin to Tailwind CSS configuration

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html data-theme="light" lang="en">
       <body className={poppins.className}>
         <Toaster/>
         {children}</body>
