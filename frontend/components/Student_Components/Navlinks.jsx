2b2fc240195f408ab78ff8b887e2b8d39bc650ef
iPranav-Patil <spydycool@gmail.com>
2024-04-14 08:07:27 +0530

    Navlinks

--- frontend/components/Student_Components/Navlinks.jsx
+++ frontend/components/Student_Components/Navlinks.jsx
@@ -1,25 +1,32 @@
 import Link from "next/link";
+import { MdOutlineDashboard } from "react-icons/md";
+import { MdLeaderboard } from "react-icons/md";
+import { IoIosChatboxes } from "react-icons/io";
+import { MdRedeem } from "react-icons/md";
+import { MdLibraryBooks } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 
 const links = [
-  { href: "/dashboard", label: "DashBoard" },
-  { href: "/leaderboard", label: "Leaderboard" },
-  { href: "/chat", label: "Chatbot" },
-  { href: "/redeem", label: "Redeem" },
-  { href: "/assignments", label: "Assignments" },
-  { href: "/profile", label: "profile" },
+  { href: "/dashboard", label: "DashBoard", img: <MdOutlineDashboard /> },
+  { href: "/leaderboard", label: "Leaderboard" , img: <MdLeaderboard />},
+  { href: "/chat", label: "Chatbot" , img: <IoIosChatboxes /> },
+  { href: "/redeem", label: "Redeem" , img: <MdRedeem />},
+  { href: "/assignments", label: "Assignments" , img: <MdLibraryBooks />},
+  { href: "/profile", label: "profile" , img: <CgProfile /> },
 ];
 
 const NavLinks = () => {
-
-  
   return (
-    <ul className="menu text-base-content">
+    <ul className="menu text-2xl font-medium">
       {links.map((link) => {
         return (
           <li key={link.href}>
-            <Link href={`/student${link.href}`} className="capitalize">
-              {link.label}
-            </Link>
+            <div className="flex my-2">
+              {link.img}
+              <Link href={`/student${link.href}`} className="capitalize">
+                {link.label}
+              </Link>
+            </div>
           </li>
         );
       })}
--- frontend/components/Techer_Components/Navlinks.jsx
+++ frontend/components/Techer_Components/Navlinks.jsx
@@ -1,22 +1,30 @@
 import Link from "next/link";
+import { MdOutlineDashboard } from "react-icons/md";
+import { MdOutlineLibraryBooks } from "react-icons/md";
+import { FaFileSignature } from "react-icons/fa";
+import { MdQuiz } from "react-icons/md";
 
 const links = [
-  { href: "/dashboard", label: "DashBoard" },
-  { href: "/assignments", label: "Assignments" },
-  { href: "/attendance", label: "Attendance" },
-  { href: "/addquiz", label: "Add a Quiz" },
+  { href: "/dashboard", label: "DashBoard" , img: <MdOutlineDashboard /> },
+  { href: "/assignments", label: "Assignments" , img:<MdOutlineLibraryBooks />  },
+  { href: "/attendance", label: "Attendance" , img: <FaFileSignature /> },
+  { href: "/addquiz", label: "Add a Quiz" , img: <MdQuiz />
+},
 
 ];
 
 const NavLinks = () => {
   return (
-    <ul className="menu text-base-content">
+    <ul className="menu  text-2xl font-medium">
       {links.map((link) => {
         return (
           <li key={link.href}>
+            <div className="flex my-2">
+              {link.img}
             <Link href={`/teacher${link.href}`} className="capitalize">
               {link.label}
             </Link>
+            </div>
           </li>
         );
       })}
